23ee6c2f39e6de5782a40048ebf471ddf3c10ae0
Jenkins Automation Server <jenkins@i-0a58c4edd5c7087b4.instanceid.glomex.cloud>
2017-05-26 07:39:07 +0000

    Bump version: 0.1.28 → 0.1.29

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ author = u'glomex DevOps team & friends'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.28'
+version = '0.1.29'
 # The full version, including alpha/beta/rc tags.
 #release = '0.0.74'
 release = version
