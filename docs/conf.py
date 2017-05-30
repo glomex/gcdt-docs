5f989ad31631229eab2bb7af62ae2e06c992e6ae
glomex-sre-deploy <sre-chapter@de.glomex.com>
2017-05-30 17:01:39 +0000

    Bump version: 0.1.357 → 0.1.358

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ author = u'glomex DevOps team & friends'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.357'
+version = '0.1.358'
 # The full version, including alpha/beta/rc tags.
 #release = '0.0.74'
 release = version
