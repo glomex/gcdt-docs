8a05321e42ca9e3a4151f6989fda218697c08b66
Jenkins Continuous Integration Server <sre-chapter@de.glomex.com>
2017-06-09 14:07:14 +0000

    Bump version: 0.1.394 → 0.1.395

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ author = u'glomex DevOps team & friends'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.394'
+version = '0.1.395'
 # The full version, including alpha/beta/rc tags.
 #release = '0.0.74'
 release = version
